9a6f8dbb08c05342ed538c42745b61731ace2729
laihoangdo <laihoangdo0506@gmail.com>
2026-09-11 22:46:23 +0700

    Update allowed origin for CORS configuration

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const allowedOrigins = [
     'http://localhost:5500',
     'http://127.0.0.1:5500',
     'http://localhost:3000',
-    'https://laihoangdo.github.io/police-fe'
+    'https://laihoangdo.github.io'
 ];
 
 app.use(cors({
