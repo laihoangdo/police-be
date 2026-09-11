1d0fde31deeb0b5918e4ccd7313e1c8ed6e74056
laihoangdo <laihoangdo0506@gmail.com>
2026-09-11 22:44:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const app = express();
 const allowedOrigins = [
     'http://localhost:5500',
     'http://127.0.0.1:5500',
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'https://laihoangdo.github.io/police-fe'
 ];
 
 app.use(cors({
